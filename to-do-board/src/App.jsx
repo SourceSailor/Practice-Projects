d8358735842fa842c7de22f24db5857e3817c1be
Kyle Pickard <kylepickard95@gmail.com>
2024-10-04 19:18:58 -0700

    Added app functionality
    Created Add Task function to add tasks to the board.
    
    Created delete function to remove tasks from the board.
    
    Created completed function that strikes a line through the task text when pressed

--- to-do-board/src/App.jsx
+++ to-do-board/src/App.jsx
@@ -1,9 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  const taskRef = useRef("");
+
+  function onAddTask() {
+    let taskText = taskRef.current.value;
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: tasks.length + 1,
+        text: taskText,
+        complete: false,
+      },
+    ]);
+    return (taskRef.current.value = "");
+  }
+
+  function onDeleteTask(id) {
+    const updatedTaskList = tasks.filter((prevList) => prevList.id !== id);
+    setTasks(updatedTaskList);
+  }
+
+  function onComplete(id) {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, complete: !task.complete } : task
+      )
+    );
+  }
+
+  console.log(tasks);
 
   const btnCss =
     "flex w-24 px-2 py-2 rounded-lg align-middle justify-center bg-slate-300 hover:bg-slate-600 hover:text-white";
@@ -19,21 +49,45 @@ function App() {
           type="text"
           name="addTaskInp"
           id="addTaskInp"
+          ref={taskRef}
         />
-        <button className={btnCss}>Enter</button>
+        <button onClick={onAddTask} className={btnCss}>
+          Enter
+        </button>
       </div>
 
+      {/* Tasks */}
       <section className="flex flex-row justify-center align-middle">
-        <div className="flex flex-col justify-between border-gray-400 border-2 w-[400px] h-[200px] py-5 align-middle ">
-          <h4 className="flex-grow flex items-center justify-center">
-            Task Text
-          </h4>
-          <div className="flex flex-row gap-3 align-middle justify-center">
-            <button className={btnCss}>Delete</button>
-            <button className={btnCss}>Edit</button>
-            <button className={btnCss}>Complete</button>
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="flex flex-col justify-between border-gray-400 border-2 w-[400px] h-[200px] py-5 align-middle rounded-lg"
+          >
+            <h4
+              className={`flex-grow flex items-center justify-center ${
+                task.complete && "line-through"
+              }`}
+            >
+              {task.text}
+            </h4>
+
+            {/* Buttons */}
+            <div className="flex flex-row gap-3 align-middle justify-center">
+              {/* DELETE */}
+              <button onClick={() => onDeleteTask(task.id)} className={btnCss}>
+                Delete
+              </button>
+
+              {/* EDIT */}
+              <button className={btnCss}>Edit</button>
+
+              {/* COMPLETE */}
+              <button onClick={() => onComplete(task.id)} className={btnCss}>
+                {!task.complete ? "Complete" : "Incomplete"}
+              </button>
+            </div>
           </div>
-        </div>
+        ))}
       </section>
     </main>
   );
